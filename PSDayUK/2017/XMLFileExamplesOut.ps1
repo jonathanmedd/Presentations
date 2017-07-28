46af4c094e769f026722e88ddfc6d56f23c5b0d9
Jonathan Medd <jon_medd@hotmail.com>
2017-07-28 17:09:50 +0100

    Extra content for XML out

--- PSDayUK/2017/XMLFileExamplesOut.ps1
+++ PSDayUK/2017/XMLFileExamplesOut.ps1
@@ -85,5 +85,60 @@ function Out-XML {
 # --- Use the Out-XML function to output to an XML file
 $XML4 | Out-XML -FilePath 'C:\Users\Jonathan\Documents\Development\Presentations\PSDayUK\2017\Data\Example4.xml'
 
-# --- Update data in XML before outputting
+# --- Update data in XML using XPath method
+$XML4.SelectSingleNode('//food[name="Strawberry Belgian Waffles"]')
 
+$XML4.SelectSingleNode('//food[name="Strawberry Belgian Waffles"]/price').InnerText = '$8.95'
+
+$XML4.SelectSingleNode('//food[name="Strawberry Belgian Waffles"]')
+
+# --- Update data in XML using Object dot notation
+$XML4.breakfast_menu.food | where name -eq "French Toast"
+
+($XML4.breakfast_menu.food | where name -eq "French Toast").calories = '650'
+
+$XML4.breakfast_menu.food | where name -eq "French Toast"
+
+# --- Add a node by creating a new element
+$food = $XML4.CreateElement('food')
+$food.SetAttribute('name','Cereal')
+$food.SetAttribute('price','$2.95')
+$food.SetAttribute('description','A choice of five different cereals plus milk')
+$food.SetAttribute('calories','400')
+
+$XML4.breakfast_menu.AppendChild($food)
+
+$XML4.breakfast_menu.food
+
+# --- Add a node by cloning an existing node. Insert it after a specific node rather than at the end
+$newnode = $XML4.breakfast_menu.food[0].CloneNode($true)
+$newnode.name = 'Pancakes'
+$newnode.price = '$4.95'
+$newnode.description = 'Four pancakes and a choice of syrup, lemon juice and chocolate'
+$newnode.calories = '550'
+$newnode
+
+[void]$XML4.breakfast_menu.InsertAfter($newnode, ($XML4.breakfast_menu.food | where name -eq "French Toast"))
+
+$XML4.breakfast_menu.food
+
+# --- Create a node via Here-String and append to the existing XML. We first need to convert it by using ImportNode
+# --- Otherwise PowerShell will throw an error "The node to be inserted is from a different document context."
+[xml]$newnode2 = @'
+    <food>
+        <name>Chocolate Waffles</name>
+        <price>$6.95</price>
+        <description>Two of our famous Belgian Waffles with plenty of chocolate</description>
+        <calories>800</calories>
+    </food>
+'@
+$newnode2.food
+
+[void]$XML4.DocumentElement.AppendChild($XML4.ImportNode($newnode2.food, $true))
+
+$XML4.breakfast_menu.food
+
+# --- Remove a node. Find the node to remove then call the RemoveChild method from the parent
+$XML4.breakfast_menu.food | where name -eq "Cereal" | ForEach-Object {[void]$_.ParentNode.RemoveChild($_)}
+
+$XML4.breakfast_menu.food
