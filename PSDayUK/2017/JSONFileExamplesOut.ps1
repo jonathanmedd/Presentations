16f01390011216f5db6b7aaabdfba48b88697409
Jonathan Medd <jon_medd@hotmail.com>
2017-08-08 16:32:22 +0100

    Updated examples with line numbers

--- PSDayUK/2017/JSONFileExamplesOut.ps1
+++ PSDayUK/2017/JSONFileExamplesOut.ps1
@@ -1,12 +1,12 @@
 # --- Output to a JSON file
 Get-ChildItem * | ConvertTo-Json | Out-File .\Data\Example2.json
 
-# --- Depth parameter gotcha: default is 2. This means we have lines like this:
+# --- Depth parameter gotcha: default is 2. This means we have lines like this (54):
 # --- "Credential":  "System.Management.Automation.PSCredential",
 # --- Where the object has not been expanded
 Get-ChildItem * | ConvertTo-Json -Depth 3 | Out-File .\Data\Example3.json
 
-# --- It gets expanded to:
+# --- It gets expanded to (107):
 <#                        
 "Credential":  {
     "UserName":  null,
