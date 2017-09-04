813e50f7350ffde96fde2d8588360fc8e7e5d1b4
Jonathan Medd <jon_medd@hotmail.com>
2017-09-04 16:25:22 +0100

    Updates

--- PSDayUK/2017/ExcelFileExamplesOut.ps1
+++ PSDayUK/2017/ExcelFileExamplesOut.ps1
@@ -4,35 +4,11 @@ Get-Service | Select-Object Name,DisplayName,Status,StartType | Export-Excel -Pa
 # --- Export to Excel specifying Worksheet name and format the file to column width
 Get-Service | Select-Object Name,DisplayName,Status,StartType | Export-Excel -WorkSheetname 'Services' -Path .\Data\Example3.xlsx -AutoSize -Show
 
-# --- Export data in an object to Excel via the pipeline. Ignore number types. Most parameters are supplied via splatting
-# --- In the result look at F2 and G2
-$ExcelParams = @{
-
-    WorksheetName = 'Numbers'
-    Path = '.\Data\Example4.xlsx'
-    AutoSize = $true
-    Show = $true
-}
-[PSCustomObject]@{
-    Date      = Get-Date
-    Formula1  = '=SUM(F2:G2)'
-    String1   = 'My String'
-    String2   = 'a'
-    IPAddress = '10.10.25.5'
-    Number1   = '07670'
-    Number2   = '0,26'
-    Number3   = '1.555,83'
-    Number4   = '1.2'
-    Number5   = '-31'
-    PhoneNr1  = '+32 44'
-    PhoneNr2  = '+32 4 4444 444'
-    PhoneNr3  =  '+3244444444'
-} | Export-Excel @ExcelParams -NoNumberConversion *
-
 # --- Tidy up check
-if (Get-Item .\Data\Example5.xlsx) {Remove-Item .\Data\Example5.xlsx -Force -Confirm:$false}
+if (Get-Item .\Data\Example5.xlsx -ErrorAction SilentlyContinue) {Remove-Item .\Data\Example5.xlsx -Force -Confirm:$false}
 
-# --- Get process data and include a chart
+# --- Get process data and include a chart.
+# --- Most parameters are supplied via splatting
 $Data = Invoke-Sum (Get-Process) company handles,pm,VirtualMemorySize
 
 $Chart = New-ExcelChart -Title Stats `
