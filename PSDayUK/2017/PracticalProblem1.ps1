3ac59c85400df4081768a901bb79b6856ca7117d
Jonathan Medd <jon_medd@hotmail.com>
2017-09-04 14:48:18 +0100

    Updated version of ConvertTo-Hashtable

--- PSDayUK/2017/PracticalProblem1.ps1
+++ PSDayUK/2017/PracticalProblem1.ps1
@@ -50,24 +50,40 @@ Measure-Command {
 # --- Convert PSCustomObject to a Hashtable
 function ConvertTo-Hashtable
 {
-    [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [psobject[]] $InputObject
+        [Parameter(ValueFromPipeline)]
+        $InputObject
     )
 
     process
     {
-        foreach ($object in $InputObject)
+        if ($null -eq $InputObject) { return $null }
+
+        if ($InputObject -is [System.Collections.Hashtable]) {
+            return $InputObject
+        }
+        elseif ($InputObject -is [System.Collections.IEnumerable] -and $InputObject -isnot [string])
+        {
+            $collection = @(
+                foreach ($object in $InputObject) { ConvertTo-Hashtable $object }
+            )
+
+            Write-Output -NoEnumerate $collection
+        }
+        elseif ($InputObject -is [psobject])
         {
             $hash = @{}
-            
-            foreach ($property in $object.PSObject.Properties)
+
+            foreach ($property in $InputObject.PSObject.Properties)
             {
-                $hash[$property.Name] = $property.Value
+                $hash[$property.Name] = ConvertTo-Hashtable $property.Value
             }
 
             $hash
+        }
+        else
+        {
+            $InputObject
         }
     }
 }
