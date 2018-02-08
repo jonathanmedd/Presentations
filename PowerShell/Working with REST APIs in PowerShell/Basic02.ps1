00fc8c9a6898d0bd2d5d600c63a4d38edd7c15f6
Jonathan Medd <jon_medd@hotmail.com>
2018-02-08 18:11:43 +0000

    Initial draft for Basic02

---
+++ PowerShell/Working with REST APIs in PowerShell/Basic02.ps1
@@ -0,0 +1,49 @@
+# --- Test for PS 5.1
+if ($ISCoreCLR){
+
+    throw "Switch to PS 5.1"
+}
+
+# --- Deal with untrusted certificates in PS 5.1
+# --- Allows all untrusted certs in this session
+# --- Not a good idea, unless you are certain about this!
+Add-Type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+# --- Use TLS 1.2, otherwise it may default to use SSL 3.0
+[System.Net.ServicePointManager]::SecurityProtocol += [System.Net.SecurityProtocolType]::Tls12
+
+# --- Basic Authentication
+
+# --- Get credentials
+$vROCredential = Get-Credential
+
+$Username = $vROCredential.UserName
+$ConnectionPassword = $vROCredential.GetNetworkCredential().Password
+
+# --- Set Encoded Password
+$Auth = $Username + ':' + $ConnectionPassword
+$Encoded = [System.Text.Encoding]::UTF8.GetBytes($Auth)
+$EncodedPassword = [System.Convert]::ToBase64String($Encoded)
+
+Write-Host "The encoded password is: $EncodedPassword" -ForegroundColor Green
+
+# --- Set the headers
+$Headers = @{
+
+    "Accept"="application/json";
+    "Content-Type" = "application/json";
+    "Authorization" = "Basic $EncodedPassword";
+}
+
+$Headers
