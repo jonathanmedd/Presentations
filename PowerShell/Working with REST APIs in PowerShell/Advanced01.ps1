d2c6cb444384c6a2045f4727c218f22b7fa5838c
Jonathan Medd <jon_medd@hotmail.com>
2018-02-13 16:54:32 +0000

    Updates

--- PowerShell/Working with REST APIs in PowerShell/Advanced01.ps1
+++ PowerShell/Working with REST APIs in PowerShell/Advanced01.ps1
@@ -21,4 +21,14 @@ Invoke-WebRequest @Params
 $Response = Invoke-WebRequest 'http://httpbin.org/get'
 $Response.BaseResponse.GetType().FullName
 
-$Response | Get-Member
+# --- Potential issue if your code references properties that have changed
+$Response | Get-Member -MemberType Property
+
+# --- In 5.1 you could do this, but it errors in 6.0
+$Response.BaseResponse.Headers.GetValues('Content-Type')
+
+# --- Instead you need to do this in 6.0
+$Response.BaseResponse.Content.Headers.GetValues('Content-Type')
+
+# --- Error Handling
+Invoke-WebRequest 'http://httpbin.org/status/418'
